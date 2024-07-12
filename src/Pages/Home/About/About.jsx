311bfbd87d91dae3a6255b684b4b006456cdea7f
rojin akter <rojinaakter.2795@gmail.com>
2024-07-12 19:45:51 +0600

    about-us section added

---
+++ src/Pages/Home/About/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import parts from '../../../assets/images/about_us/parts.jpg'
+import person from '../../../assets/images/about_us/person.jpg'
+
+const About = () => {
+    return (
+      <div>
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col lg:flex-row">
+            <div className="lg:1/2 relative">
+              <img src={person} className=" w-3/4 rounded-lg shadow-2xl" />
+              <img
+                className="w-1/2 absolute right-5 -bottom-14"
+                src={parts}
+                alt=""
+              />
+            </div>
+            <div className="lg:1/2 sm: mt-20 ">
+              <h1 className="text-5xl font-bold">Box Office News!</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+            \
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default About;
